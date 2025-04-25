efa495b80282e9c3f39f878a33d0caa79402ba24
Diego Dalairac <ddalairac@perceptyx.com>
2025-04-25 17:06:30 -0300

    Add some comments

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Set the base URL for the project, use for github pages
   base: '/front-end-assessment/',
   server: {
+    // Open the browser when the server starts
     open: true,
   },
   plugins: [react()],
   css: {
+    // Configuration to resolve the warning: "Deprecation [legacy-js-api]:
+    // The legacy JS API is deprecated and will be removed in Dart Sass 2.0.0."
+    // This tells Vite to use the modern Sass compiler API
     preprocessorOptions: {
       scss: {
         api: 'modern-compiler'
