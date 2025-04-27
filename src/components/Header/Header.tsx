e5432061d50e8bef571383d216e9f71fac60e0d5
Diego Dalairac <ddalairac@perceptyx.com>
2025-04-27 20:03:24 -0300

    Screen reader fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,51 +3,50 @@ import './Header.scss';
 interface IHeaderProps {
   moves: number;
   timeElapsed: string;
-  isModalOpen: boolean;
   onReset: () => void;
   setOpenModal: (isModalOpen: boolean) => void;
+  isModalOpen: boolean;
 }
 
 const Header = ({
   moves,
   timeElapsed,
-  isModalOpen,
   onReset,
   setOpenModal,
+  isModalOpen, // To prevent buttons from being accessible to screen readers when the modal is open
 }: IHeaderProps) => {
   return (
     <>
       <header className='header' data-testid='game-header'>
         <h1>Memory Card Game</h1>
-        {/* If the modal is open, don't render the controls */}
-        {!isModalOpen && (
-          <div className='header-controls'>
-            <button
-              className='reset-button'
-              data-testid='reset-game-button'
-              onClick={onReset}
-              disabled={timeElapsed === '0:00'}
-            >
-              Reset Game
-            </button>
-            <span className='moves' data-testid='moves-counter'>
-              Moves: {moves}
-            </span>
-            <span className='time' data-testid='time-counter'>
-              Time: {timeElapsed}
-            </span>
-            <button
-              className='reset-button'
-              data-testid='change-level-button'
-              onClick={() => {
-                onReset();
-                setOpenModal(true);
-              }}
-            >
-              Change level
-            </button>
-          </div>
-        )}
+        <div className='header-controls' aria-hidden={isModalOpen}>
+          <button
+            className='reset-button'
+            data-testid='reset-game-button'
+            onClick={onReset}
+            disabled={timeElapsed === '0:00'}
+            tabIndex={isModalOpen ? -1 : 0}
+          >
+            Reset Game
+          </button>
+          <span className='moves' data-testid='moves-counter'>
+            Moves: {moves}
+          </span>
+          <span className='time' data-testid='time-counter'>
+            Time: {timeElapsed}
+          </span>
+          <button
+            className='reset-button'
+            data-testid='change-level-button'
+            onClick={() => {
+              onReset();
+              setOpenModal(true);
+            }}
+            tabIndex={isModalOpen ? -1 : 0}
+          >
+            Change level
+          </button>
+        </div>
       </header>
     </>
   );
